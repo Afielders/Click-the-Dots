b9c2c30b58ffcdedd8dcebaf7786e0b23766f6c1
Alex Fielders <af270024@ohio.edu>
2026-03-27 10:34:36 -0400

    Added score and started timer

--- Click the Dots/Assets/GameLogic.cs
+++ Click the Dots/Assets/GameLogic.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using UnityEngine;
+using TMPro;
 
 public class GameLogic : MonoBehaviour
 {
@@ -8,13 +9,24 @@ public class GameLogic : MonoBehaviour
     // Spawning Dots
     public GameObject dot;
     public float time_between_spawns = 1f;
-    public float dot_timer = 0.0f;
+    private float dot_timer = 0.0f;
+
+
+    //Score
+    private int score = 0;
+    public TMP_Text score_text;
+
+    //GameTimer
+    private float game_timer =60; //In Seconds.
+    public TMP_Text game_timer_text;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // Start timeer.
         dot_timer = 0.5f;
+        //Set the default text at the beginning of the game.
+        score_text.text = "Score: 0";
     }
 
     // Update is called once per frame
@@ -31,6 +43,12 @@ public class GameLogic : MonoBehaviour
 
             if(hit.collider != null)
             {
+                //Add the clicked dots point value.
+                score += hit.collider.gameObject.GetComponent<Dot>().point_value;
+
+                //Set the score_text's text string to the current score value.
+                score_text.text = "Score: " + score;
+
                 //If so destroy the game object that has the collider.
                 Destroy(hit.collider.gameObject);
             }
@@ -54,7 +72,17 @@ public class GameLogic : MonoBehaviour
     }
 
     private void SpawnDot()
-    { 
-    
+    {
+        GameObject new_dot = Instantiate(dot);
+        // Find a random x/y position in screen space.
+        int x_pos = Random.Range(0, Cam.scaledPixelWidth);
+        int y_pos = Random.Range(0, Cam.scaledPixelHeight);
+        // Convert the position to world space.
+        Vector3 spawn_point = new Vector3(x_pos, y_pos, 0);
+        spawn_point = Cam.ScreenToWorldPoint(spawn_point);
+        spawn_point.z = 0;
+
+        //Move new dot to new spawn point.
+        new_dot.transform.position = spawn_point;
     }
 }
